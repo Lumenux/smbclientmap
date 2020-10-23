b67fd6669835cc95ab6d655b2824113716a46292
Yottai <72920563+Yottai@users.noreply.github.com>
2020-10-23 11:08:58 +0200

    Update smbclientmap.sh

--- smbclientmap.sh
+++ smbclientmap.sh
@@ -9,29 +9,29 @@ ip=$1
 echo ""
 echo "### Testing $ip ###"
 
-stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N -g -L //$ip >output:g.txt 2>&1
+stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N -g -L //$ip >output.txt 2>&1
 echo ""
-cat output:g.txt
+cat output.txt
 echo ""
 
 # A typical line from smbclient -L (using the -g parameter) looks like this:
 # Disk|C$|Default share
 
-for share in $(grep -E -o "Disk\|[^\|]+" output:g.txt | cut -c 6-); do
+for share in $(grep -E -o "Disk\|[^\|]+" output.txt | cut -c 6-); do
     echo "# Testing //$ip/$share #"
-    stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "dir" >output:g.txt 2>&1
+    stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "dir" >output.txt 2>&1
     status=$?
     if [ "$status" -eq "0" ]; then
         testdir=test_$(shuf -i 100000-999999 -n 1)
-        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "mkdir $testdir" >output:g.txt 2>&1
-        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "dir" >output:g.txt 2>&1
-        cat output:g.txt
-        if grep -q ${testdir} output:g.txt; then
+        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "mkdir $testdir" >output.txt 2>&1
+        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "dir" >output.txt 2>&1
+        cat output.txt
+        if grep -q ${testdir} output.txt; then
             echo "=> WRITE SUCCESS (${testdir} directory)"
         fi
-        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "rmdir $testdir" >output:g.txt 2>&1
+        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "rmdir $testdir" >output.txt 2>&1
         echo "=> READ SUCCESS"
     fi
 done
 
-[ -e output:g.txt ] && rm -f temp.txt
+[ -e output.txt ] && rm -f output.txt
