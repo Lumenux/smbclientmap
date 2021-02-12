82bd62434921c2464ca504df44e4f2a306293e37
securitytime <72920563+securitytime@users.noreply.github.com>
2021-02-12 10:54:31 +0100

    Update smbclientmap.sh

--- smbclientmap.sh
+++ smbclientmap.sh
@@ -6,10 +6,13 @@ if [ "$#" -ne "1" ]; then
 fi
 ip=$1
 
+# Create an empty config file, so we don't get the warning "Can't load /etc/samba/smb.conf - run testparm to debug it"
+touch /tmp/smbclient.conf
+
 echo ""
 echo "### Testing $ip ###"
 
-stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N -g -L //$ip >temp.txt 2>&1
+stdbuf -i0 -o0 -e0 smbclient -s /tmp/smbclient.conf --option='client min protocol=NT1' -N -g -L //$ip >temp.txt 2>&1
 echo ""
 cat temp.txt
 echo ""
@@ -19,17 +22,17 @@ echo ""
 
 for share in $(grep -E -o "Disk\|[^\|]+" temp.txt | cut -c 6-); do
     echo "# Testing //$ip/$share #"
-    stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "dir" >temp.txt 2>&1
+    stdbuf -i0 -o0 -e0 smbclient -s /tmp/smbclient.conf --option='client min protocol=NT1' -N //$ip/$share -c "dir" >temp.txt 2>&1
     status=$?
     if [ "$status" -eq "0" ]; then
         testdir=test_$(shuf -i 100000-999999 -n 1)
-        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "mkdir $testdir" >temp.txt 2>&1
-        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "dir" >temp.txt 2>&1
+        stdbuf -i0 -o0 -e0 smbclient -s /tmp/smbclient.conf --option='client min protocol=NT1' -N //$ip/$share -c "mkdir $testdir" >temp.txt 2>&1
+        stdbuf -i0 -o0 -e0 smbclient -s /tmp/smbclient.conf --option='client min protocol=NT1' -N //$ip/$share -c "dir" >temp.txt 2>&1
         cat temp.txt
         if grep -q ${testdir} temp.txt; then
             echo "=> WRITE SUCCESS (${testdir} directory)"
         fi
-        stdbuf -i0 -o0 -e0 smbclient --option='client min protocol=NT1' -N //$ip/$share -c "rmdir $testdir" >temp.txt 2>&1
+        stdbuf -i0 -o0 -e0 smbclient -s /tmp/smbclient.conf --option='client min protocol=NT1' -N //$ip/$share -c "rmdir $testdir" >temp.txt 2>&1
         echo "=> READ SUCCESS"
     fi
 done
